7a09fdc0fdb9f8f6f7df91c89d52dc4e4ebaf8fc
reema <zkreema@icloud.com>
2025-12-24 14:45:50 +0300

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Reema Alkhelaiwi on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
